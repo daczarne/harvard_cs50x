85a3db258abce0fae70da83298d995ada8afca6b
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-12 01:14:41 -0300

    mario

---
+++ week_1/lecture-notes/mario.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  // Get positive integer from user
+  int n;
+  do
+  {
+    n = get_int("Width:");
+  } while (n < 1);
+
+  );
+
+  // Print question marks
+  for (int i = 0; i < n; i++)
+  {
+    print("?")
+  }
+  print("\n");
+}
