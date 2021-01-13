f74f63c0e545ee0ce48804087653d8ea05b583f8
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-12 22:08:27 -0300

    population

--- week_1/lab_1/population.c
+++ week_1/lab_1/population.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 // Prototype
-int get_int_greater_than_nine(void);
-int get_int_greater_than_start(int start);
+int get_int_greater_than(string prompt_message, int number);
 
 int main(void)
 {
   // TODO: Prompt for start size
-  int start = get_int_greater_than_nine();
+  int start = get_int_greater_than("Start size: ", 9);
 
   // TODO: Prompt for end size
-  int end = get_int_greater_than_start(start);
+  int end = get_int_greater_than("End size: ", start);
 
   // TODO: Calculate number of years until we reach threshold
   int llama_stock = start;
@@ -20,30 +19,19 @@ int main(void)
   while (llama_stock < end)
   {
     llama_stock = llama_stock + llama_stock / 3 - llama_stock / 4;
-
     n++;
   }
 
   // TODO: Print number of years
-  printf("%i\n", n);
+  printf("Years: %i\n", n);
 }
 
-int get_int_greater_than_nine(void)
+int get_int_greater_than(string prompt_message, int number)
 {
   int n;
   do
   {
-    n = get_int("Start size: ");
-  } while (n < 9);
-  return n;
-}
-
-int get_int_greater_than_start(int start)
-{
-  int n;
-  do
-  {
-    n = get_int("End size: ");
-  } while (n < start);
+    n = get_int("%s", prompt_message);
+  } while (n < number);
   return n;
 }
