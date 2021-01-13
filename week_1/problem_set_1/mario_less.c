67fef08c032bf51cb4dcc900811bfd5b2de5e612
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-13 18:56:30 -0300

    mario less

--- week_1/problem_set_1/mario_less.c
+++ week_1/problem_set_1/mario_less.c
@@ -1,18 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Prototype
+int get_height(void);
+
 int main(void)
 {
   // Get height from the user
-  int height = get_int("Height: ");
+  int height = get_height();
 
   // Build pyramid
   for (int i = 0; i < height; i++)
   {
-    for (int j = 0; j <= i; j++)
+    for (int j = 0; j < height; j++)
     {
-      printf("#");
+      if (j >= (height - (i + 1)))
+      {
+        printf("#");
+      }
+      else
+      {
+        printf(" ");
+      }
     }
     printf("\n");
   }
+}
+
+// Get height function
+int get_height(void)
+{
+  int n;
+  do
+  {
+    n = get_int("Height: ");
+  } while (!(0 < n && n < 9));
+  return n;
 }
