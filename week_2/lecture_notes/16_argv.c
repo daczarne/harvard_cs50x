96ed4eb8d38bb53dbfa80e9adaa56c12752eb9a8
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-17 18:00:42 -0300

    argv

---
+++ week_2/lecture_notes/16_argv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cd50.h>
+
+int main(int argc, string argv[])
+{
+  if (argc == 2)
+  {
+    printf("hello, %s\n", argv[1]);
+  }
+  else
+  {
+    printf("hello, world\n");
+  }
+}
