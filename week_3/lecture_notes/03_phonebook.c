ae57007e3ebf1b57141b800d06c8067ea268338d
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-24 19:04:07 -0300

    fixes files names

--- week_3/lecture_notes/03_phonebook.c
+++ week_3/lecture_notes/03_phonebook.c
@@ -2,25 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct
-{
-  string name;
-  string number;
-} person;
-
 int main(void)
 {
-  person people[2];
-  people[0].name = "Brian";
-  people[0].number = "+1-617-495-1000";
-  people[1].name = "David";
-  people[1].number = "+1-949-468-2750";
+  string names[] = {"Brian", "David"};
+  string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
 
   for (int i = 0; i < 2; i++)
   {
-    if (strcmp(people[i].name, "David") == 0)
+    if (strcmp(names[i], "David") == 0)
     {
-      printf("Found %s\n", people[i].number);
+      printf("Found %s\n", numbers[i]);
       return 0;
     }
   }
