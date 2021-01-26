1d07bb42c4ca232e194d9fafa9a74a6bd2bda4ee
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-26 19:43:39 -0300

    address

--- week_4/lecture_notes/lecture_notes/01_address.c
+++ week_4/lecture_notes/lecture_notes/01_address.c
@@ -3,5 +3,13 @@
 int main(void)
 {
   int n = 50;
+  // Print the variable n
   printf("%i\n", n);
+
+  // Print the address of the variable n
+  // We use format type %p to get the Hex value
+  printf("%p\n", &n);
+
+  // Go to the address of the variable n, and print the content of that variable
+  printf("%i\n", *&n);
 }
