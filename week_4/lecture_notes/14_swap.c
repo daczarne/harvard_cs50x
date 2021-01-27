f89403bf5733e54a8115b9dbdc44ce1d5df8fbd1
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-26 23:35:16 -0300

    swap

--- week_4/lecture_notes/14_swap.c
+++ week_4/lecture_notes/14_swap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Prototype
-void swap(int a, int b);
+void swap(int *a, int *b);
 
 int main(void)
 {
@@ -9,13 +9,13 @@ int main(void)
   int y = 2;
 
   printf("x is %i, y is %i\n", x, y);
-  swap(x, y);
+  swap(&x, &y);
   printf("x is %i, y is %i\n", x, y);
 }
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-  int tmp = a;
-  a = b;
-  b = tmp;
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
