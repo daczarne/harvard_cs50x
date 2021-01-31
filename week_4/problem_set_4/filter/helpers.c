c88958d573d5b1e6c8879996fc0d87ee8ed49a58
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-31 02:24:11 -0300

    adds comments
    changes sepia checks for ternary op

--- week_4/problem_set_4/filter/helpers.c
+++ week_4/problem_set_4/filter/helpers.c
@@ -8,7 +8,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            // Calculate avg pixel
             int avg_rgb = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3);
+
+            // Set new pixel values
             image[i][j].rgbtRed = avg_rgb;
             image[i][j].rgbtGreen = avg_rgb;
             image[i][j].rgbtBlue = avg_rgb;
@@ -24,24 +27,22 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            // Get sepia red
             int sepia_red = round(
                 0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
-            if (sepia_red > 255)
-            {
-                sepia_red = 255;
-            }
+            sepia_red = (sepia_red > 255) ? 255 : sepia_red;
+
+            // Get sepia green
             int sepia_green = round(
                 0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
-            if (sepia_green > 255)
-            {
-                sepia_green = 255;
-            }
+            sepia_green = (sepia_green > 255) ? 255 : sepia_green;
+
+            // Get sepia blue
             int sepia_blue = round(
                 0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
-            if (sepia_blue > 255)
-            {
-                sepia_blue = 255;
-            }
+            sepia_blue = (sepia_blue > 255) ? 255 : sepia_blue;
+
+            // Build new pixels
             image[i][j].rgbtRed = sepia_red;
             image[i][j].rgbtGreen = sepia_green;
             image[i][j].rgbtBlue = sepia_blue;
