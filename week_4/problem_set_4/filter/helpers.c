ab70f2bfc6c908b49e73354b060ab1fd01c170cc
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-31 02:48:03 -0300

    reflect

--- week_4/problem_set_4/filter/helpers.c
+++ week_4/problem_set_4/filter/helpers.c
@@ -54,6 +54,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0, n = round(width / 2); j < n; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - j];
+            image[i][width - j] = temp;
+        }
+    }
     return;
 }
 
