a69cb6b9351a6d53cd2340166223fcddd83e99fc
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-01-31 02:19:05 -0300

    sepia

--- week_4/problem_set_4/filter/helpers.c
+++ week_4/problem_set_4/filter/helpers.c
@@ -20,6 +20,33 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sepia_red = round(
+                0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            if (sepia_red > 255)
+            {
+                sepia_red = 255;
+            }
+            int sepia_green = round(
+                0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            if (sepia_green > 255)
+            {
+                sepia_green = 255;
+            }
+            int sepia_blue = round(
+                0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+            if (sepia_blue > 255)
+            {
+                sepia_blue = 255;
+            }
+            image[i][j].rgbtRed = sepia_red;
+            image[i][j].rgbtGreen = sepia_green;
+            image[i][j].rgbtBlue = sepia_blue;
+        }
+    }
     return;
 }
 
