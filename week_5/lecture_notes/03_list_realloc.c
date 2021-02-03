1201032352a80d006442fb91671b4515aea0e85f
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-02 23:25:32 -0300

    list realloc

---
+++ week_5/lecture_notes/03_list_realloc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  // Assing memory for list
+  int *list = malloc(3 * sizeof(int));
+  if (list == NULL)
+  {
+    return 1;
+  }
+
+  // Assign elements
+  list[0] = 1;
+  list[1] = 2;
+  list[2] = 3;
+
+  // Allocate a new element
+  int *tmp = realloc(list, 4 * sizeof(int));
+  if (tmp == NULL)
+  {
+    free(list);
+    return 1;
+  }
+
+  // Assign new element
+  tmp[3] = 4;
+
+  // Free original list
+  free(list);
+
+  // Rename list
+  list = tmp;
+
+  // Print values
+  for (int i = 0; i < 4; i++)
+  {
+    printf("%i\n", list[i]);
+  }
+
+  // Free tmp
+  free(tmp);
+}
