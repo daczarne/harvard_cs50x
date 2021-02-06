1c4437bad2be134661e744946eb2fb0fd019204e
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-06 18:48:45 -0300

    load

--- week_5/problem_set_5/speller/dictionary.c
+++ week_5/problem_set_5/speller/dictionary.c
@@ -9,14 +9,16 @@ typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
-}
-node;
+} node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 26;
 
 // Hash table
 node *table[N];
+
+// Count the words in the dictionary
+unsigned int dict_counter = 0;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
@@ -28,6 +30,7 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
+    tolower(node.word[0]) - 97;
     // TODO
     return 0;
 }
@@ -35,8 +38,45 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // Define a "word" buffer where to store the string that is been read from the dict
+    char word[LENGTH + 1];
+    // Open de dictonary
+    FILE *dict = fopen(dictionary, "r");
+    if (dict == NULL)
+    {
+        return false;
+    }
+    // Scan a word form the dictionary
+    bool continue_scanning = true;
+    while (continue_scanning)
+    {
+        // Read the word into memroy
+        fscanf(dict, "%s", word);
+        // Create a new node
+        node n * = malloc(sizeof(node));
+        if (node == NULL)
+        {
+            return false;
+        }
+        // Hash the word
+        int hash_table_position = hash(word);
+        // Copy the content of the "word" variable into the "word" field of the "n" variable
+        strcpy(n->word, word);
+        // Assign the node to the first position of the linked-list in the position determined by the hash function
+        n->next = table[hash_table_position];
+        table[hash_table_position] = n;
+        // Augment number of words
+        dict_counter++;
+        // Check if end of file has been reached
+        if (fscanf(dict, "%s", word) == EOF)
+        {
+            // Close the dictionary
+            fclose(dict);
+            // Set flag to false to stop loop
+            continue_scanning = false;
+        }
+    }
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
