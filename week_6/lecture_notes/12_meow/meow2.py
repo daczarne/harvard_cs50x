d550104d6966a9f522159f1dadc5a1d0a4a0692f
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-07 20:53:45 -0300

    meow

---
+++ week_6/lecture_notes/12_meow/meow2.py
@@ -0,0 +1,16 @@
+# Abstraction
+
+
+def main():
+    for i in range(3):
+        meow()
+
+# Meow once
+
+
+def meow():
+    print("meow")
+
+
+if __name__ == "__main__":
+    main()
---
+++ week_6/lecture_notes/12_meow/meow3.py
@@ -0,0 +1,15 @@
+# Abstraction with parameterization
+
+
+def main():
+    meow(3)
+
+
+# Meow some number of times
+def meow(n):
+    for i in range(n):
+        print("meow")
+
+
+if __name__ == "__main__":
+    main()
