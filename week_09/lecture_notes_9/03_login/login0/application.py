26c7222f815e7de05b45bc465e4fba6e44380186
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-15 19:17:37 -0300

    login0

--- week_09/lecture_notes_9/03_login/login0/application.py
+++ week_09/lecture_notes_9/03_login/login0/application.py
@@ -6,26 +6,15 @@ app = Flask(__name__)
 
 # Configure sessions
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_TYPE"] = "filesystem"  # the hard drive
 Session(app)
 
 
 @app.route("/")
 def index():
-    if not session.get("name"):
-        return redirect("/login")
     return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        session["name"] = request.form.get("name")
-        return redirect("/")
     return render_template("login.html")
-
-
-@app.route("/logout")
-def logout():
-    session["name"] = None
-    return redirect("/")
