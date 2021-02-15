a615280714c64e676fb8b08208ef039012c9ff8e
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-15 17:15:20 -0300

    frosh05

--- week_09/lecture_notes_9/02_registration_system/froshims05/application.py
+++ week_09/lecture_notes_9/02_registration_system/froshims05/application.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 SPORTS = [
     "Dodgeball",
-    "Flag Football",
-    "Soccer",
+    "Rugby",
+    "Football",
     "Volleyball",
     "Ultimate Frisbee"
 ]
@@ -19,10 +19,8 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-
     # Validate submission
     if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
-
     # Confirm registration
     return render_template("success.html")
