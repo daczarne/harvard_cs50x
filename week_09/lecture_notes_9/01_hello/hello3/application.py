9c310f6664c307e28d8d8178d26c2734c5bf35ef
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-15 15:01:36 -0300

    hello3

---
+++ week_09/lecture_notes_9/01_hello/hello3/application.py
@@ -0,0 +1,14 @@
+# Greets user via a form using GET
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", name=request.args.get("name", "world"))
+
+
+@app.route("/greet")
+def greet():
+    return "TODO"
