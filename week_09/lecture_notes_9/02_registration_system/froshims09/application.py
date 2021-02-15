ca2aa9befbde2a41f16cec71e31aaf3d7d2ae7ca
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-15 17:39:54 -0300

    frosh09

---
+++ week_09/lecture_notes_9/02_registration_system/froshims09/application.py
@@ -0,0 +1,36 @@
+# Implements a registration form using radio buttons
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+SPORTS = [
+    "Dodgeball",
+    "Flag Football",
+    "Soccer",
+    "Volleyball",
+    "Ultimate Frisbee"
+]
+
+REGISTRANTS = {}
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    # Check form
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+    # Register user and confirm
+    REGISTRANTS[name] = sport
+    return render_template("registrants.html", registrants=REGISTRANTS)
