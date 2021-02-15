5576141aa903f2ef1bccdc847327c0848bac96f8
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-15 15:32:57 -0300

    hello8

--- week_09/lecture_notes_9/01_hello/hello8/application.py
+++ week_09/lecture_notes_9/01_hello/hello8/application.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "GET":
+        return render_template("index.html")
     if request.method == "POST":
         return render_template("greet.html", name=request.form.get("name", "world"))
-    return render_template("index.html")
