6a9f8efa9a3338372479ab90b26b83ff9899c70f
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-15 17:02:11 -0300

    frosh03

--- week_09/lecture_notes_9/02_registration_system/froshims03/application.py
+++ week_09/lecture_notes_9/02_registration_system/froshims03/application.py
@@ -8,6 +8,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST"])
 def register():
     return render_template("success.html")
